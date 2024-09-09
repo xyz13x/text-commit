78bd4296aa9b4706c9bcf3b8f7342cc7ec4408dd
wangshch07 <10997242+seeking-sincerity@user.noreply.gitee.com>
2024-09-09 19:45:36 +0800

    第一次修改

--- src/api/visualization.js
+++ src/api/visualization.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: wangsc
+ * @Date: 2024-09-07 20:45:50
+ * @LastEditTime: 2024-09-09 19:44:13
+ * @LastEditors: wangsc
+ * @Description: 
+ */
 import request from '../utils/request.js';
 
+// 获取数据接口
 export const getVisualization = () =>{
   return request({
     url: '/visualization',
